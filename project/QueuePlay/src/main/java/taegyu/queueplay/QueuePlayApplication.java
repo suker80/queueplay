d3fe380690d2d2016cee57c84779005350cbd4c5
suker <suker800@gmail.com>
2022-03-20 18:54:57 +0900

    Initial commit

---
+++ project/QueuePlay/src/main/java/taegyu/queueplay/QueuePlayApplication.java
@@ -0,0 +1,13 @@
+package taegyu.queueplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuePlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuePlayApplication.class, args);
+    }
+
+}
---
+++ project/QueuePlay/src/test/java/taegyu/queueplay/QueuePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package taegyu.queueplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuePlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
